cdb2abe0058c0cf80681dc2b46b1b9949647db77
Tenyson05 <Rojahog@hotmail.com>
2019-03-30 13:15:48 -0700

    Added Decode function

--- LSDStegnography.py
+++ LSDStegnography.py
@@ -17,17 +17,18 @@ def write_text(text_to_write, image_size):
 	return image_text
 
 
-def encode_image(encode_text, target_image="chi.jpg"):
-	target_image = Image.open(target_image)
-	red_pixels = target_image.split()[0]
-	green_pixels = target_image.split()[1]
-	blue_pixels = target_image.split()[2]
+def encode_image(encode_text, imageToEncode="chi.jpg"):
+	"""encodes a hidden message in a selected image"""
+	imageToEncode = Image.open(imageToEncode)
+	red_pixels = imageToEncode.split()[0]
+	green_pixels = imageToEncode.split()[1]
+	blue_pixels = imageToEncode.split()[2]
 
-	x_axis = target_image.size[0]
-	y_axis = target_image.size[1]
+	x_axis = imageToEncode.size[0]
+	y_axis = imageToEncode.size[1]
 
 	#draw text
-	image_text = write_text(encode_text, target_image.size)
+	image_text = write_text(encode_text, imageToEncode.size)
 	bw_encode = image_text.convert('1')
 
 	#encode text into image
@@ -50,5 +51,28 @@ def encode_image(encode_text, target_image="chi.jpg"):
 	print("done")
 
 
+def decode_image(imageToDecode="encoded_image.png"):
+	"""Decodes the hidden message in the selected image"""
+	encoded_image = Image.open(imageToDecode)
+	red_pixs = encoded_image.split()[0]
+
+	x_axis = encoded_image.size[0]
+	y_axis = encoded_image.size[1]
+
+	decoded_image = Image.new("RGB", encoded_image.size)
+	pixels = decoded_image.load()
+
+	for i in range(x_axis):
+		for j in range(y_axis):
+			if bin(red_pixs.getpixel((i, j)))[-1] == '0':
+				pixels[i, j] = (255, 255, 255)
+			else:
+				pixels[i, j] = (0,0,0)
+	decoded_image.save("decoded_image.png")
+	print("Done decoding")
+
+
+
 if __name__ == '__main__':
-	encode_image("hello world")
+	# encode_image("This is computer security!!!")
+	decode_image()
